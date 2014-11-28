31642562123f5f8ae1eba4d166dc80c1f6a4f695
hfara079 <farah2002@hotmail.com>
2014-11-27 23:03:06 -0500

    Initializer ParseFacade des le debut

--- ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/Main.java
+++ ObjetPret/src/ca/uottawa/eecs/seg2505/objetpret/Main.java
@@ -2,6 +2,8 @@ package ca.uottawa.eecs.seg2505.objetpret;
 
 import android.app.Application;
 
+import ca.uottawa.eecs.seg2505.objetpret.db.ParseFacade;
+
 import com.parse.Parse;
 
 public class Main extends Application {
@@ -11,6 +13,7 @@ public class Main extends Application {
 	    super.onCreate();
 
 	    Parse.initialize(this, "448LCfDpX4KQW2j6HHbpXbhOBLiRHm2eqCrEjHWD", "d4x4A5SJVyWYyznzqlOj6Q8MXnGdD2W6K3HkrfHl"); // Your Application ID and Client Key are defined elsewhere
+	    Delegateur.setDBFacade(new ParseFacade());
 	}
 
 }
